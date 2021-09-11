dc65c4d50e59af6608ead209eb42311bd2772fc6
Christian Habermann <haberc@users.noreply.github.com>
2021-09-11 23:00:13 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,3 +38,28 @@ module "activity_tracker_instance" {
   resource_group_id = data.ibm_resource_group.rg.id
   tags              = local.tags
 }
+  
+resource "ibm_database" "test_acc" {
+  resource_group_id = data.ibm_resource_group.group.id
+  name              = "demo-postgres"
+  service           = "databases-for-postgresql"
+  plan              = "standard"
+  location          = "eu-gb"
+  adminpassword     = "adminpassword"
+
+  whitelist {
+    address     = "${ibm_compute_vm_instance.webapp1[0].ipv4_address}/32"
+    description = ibm_compute_vm_instance.webapp1[0].hostname
+  }
+
+  tags = ["tag1", "tag2"]
+
+  // adminpassword                = "password12"
+  members_memory_allocation_mb = 3072
+  members_disk_allocation_mb   = 20480
+
+  users {
+    name     = "user123"
+    password = "password12"
+  }
+}
