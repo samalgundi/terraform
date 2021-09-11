761478099a61095b300b96e243655108d18597a5
Christian Habermann <haberc@users.noreply.github.com>
2021-09-11 23:04:57 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -47,11 +47,6 @@ resource "ibm_database" "test_acc" {
   location          = "eu-gb"
   adminpassword     = "adminpassword"
 
-  whitelist {
-    address     = "${ibm_compute_vm_instance.webapp1[0].ipv4_address}/32"
-    description = ibm_compute_vm_instance.webapp1[0].hostname
-  }
-
   tags = ["tag1", "tag2"]
 
   // adminpassword                = "password12"
